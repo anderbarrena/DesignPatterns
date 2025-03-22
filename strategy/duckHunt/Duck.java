1950a35fc78f07803cbb1d1f1178040a6050cdda
anderbarrena <ander.barrena@ehu.eus>
2025-03-22 23:18:40 +0100

    minors

--- strategy/duckHunt/Duck.java
+++ strategy/duckHunt/Duck.java
@@ -4,15 +4,13 @@ public class Duck {
     private StrategyFly strategyFly = new NormalFly();
     private StrategyQuack strategyQuack = new NormalQuack();
     public void fly(){
-	strategyFly.fly();
+	    strategyFly.fly();
     }
     public void quack(){
-	strategyQuack.quack();
+	    strategyQuack.quack();
     }
     public void changeStrategyFly(StrategyFly sv){
 	this.strategyFly=sv;
     }
-    public void changeStrategyQuack(StrategyQuack sc){
-	this.strategyQuack=sc;
-    }
+    public void changeStrategyQuack(StrategyQuack sc){this.strategyQuack=sc;}
 }
